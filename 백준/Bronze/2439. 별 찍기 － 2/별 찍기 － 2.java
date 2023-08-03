9565fbd903fec9249764b96b7dd0745c32afceed
eunpyeong114 <139293873+eunpyeong114@users.noreply.github.com>
2023-08-03 13:28:58 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 280 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = n - 1 - i; j > 0; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i + 1; j++) {
+				if (j == i + 1) {
+					System.out.println("*");
+				} else {
+					System.out.print("*");
+				}
+			}
+		}
+	}
+}

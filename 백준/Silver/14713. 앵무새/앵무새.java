b558d13bcf6361df3079544bf3551304a14fbf56
eunpyeong114 <139293873+eunpyeong114@users.noreply.github.com>
2023-09-04 12:44:13 +0900

    [Silver II] Title: 앵무새, Time: 388 ms, Memory: 27804 KB -BaekjoonHub

--- 백준/Silver/14713. 앵무새/앵무새.java
+++ 백준/Silver/14713. 앵무새/앵무새.java
@@ -1,45 +1,74 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		// 앵무새들의 입력값 받기 위한 스택
-		Stack[] words = new Stack[N];
-		sc.nextLine();
-		// 각 앵무새들의 입력값 스택에 할당
+
+		int N = sc.nextInt(); // 앵무새의 수
+		sc.nextLine(); // 개행문자 처리
+
+		// Queue N개 선언
+		Queue<String>[] queues = new Queue[N];
+
 		for (int i = 0; i < N; i++) {
-			String[] input = sc.nextLine().split(" ");
-			Stack<String> inputStack = new Stack<>();
-			for (int j = 0; j < input.length; j++) {
-				inputStack.push(input[j]);
+			queues[i] = new LinkedList<>();
+			String sentence = sc.nextLine();
+			String[] words = sentence.split(" ");
+
+			for (int j = 0; j < words.length; j++) {
+				queues[i].add(words[j]);
 			}
-			words[i] = inputStack;
-		}
-		// 받아적은 단어들 스택에 할당
-		String[] record = sc.nextLine().split(" ");
-		Stack<String> records = new Stack<>();
-		for (int i = 0; i < record.length; i++) {
-			records.push(record[i]);
 		}
 
-		for (int i = records.size() - 1; i >= 0; i--) {
-			for (int j = 0; j < words.length; j++) {
-				if(words[j].isEmpty()) {
-					continue;
-				} else if(words[j].peek().equals(records.peek())) {
-					words[j].pop();
-					records.pop();
+
+		String L = sc.nextLine(); // 입력받는 문장 L
+		String[] tmp = L.split(" "); // tmp에 split으로 문장L을 단어로 나눠서 배열에 저장
+
+		int sum = 0;
+		for(int i = 0; i<N; i++) {
+			sum+=queues[i].size();
+		}
+		
+		if(sum!=tmp.length) {
+			System.out.println("Impossible");
+			return;
+		}
+		// tmp 단어의 갯수만큼 반복을 진행
+		for (int i = 0; i < tmp.length; i++) {
+			for (int j = 0; j < N; j++) { // queue N개 확인
+				if (!queues[j].isEmpty() && tmp[i].equals(queues[j].peek())) { // queue가 빈칸 아니고, 단어의 해당 i번째가 queue n개의												// j번째의 가장 위에 있는 부분이 같을 경우
+					queues[j].remove(); // queue j번째 제거
 					break;
 				}
 			}
+		} // tmp 갯수만큼 반복문 끝
+
+		// 중간확인 (queue의 데이터와 size)
+//		for (int i = 0; i < N; i++) {
+//			System.out.println(queues[i]);
+//			System.out.println(queues[i].size());
+//		}
+
+		boolean isflag = true; // 판단하는 boolean값
+
+		for (int i = 0; i < N; i++) {
+			if (queues[i].size() != 0) { // queue에 남은 단어가 하나라도 있으면
+				isflag = false; // flag는 false로 변경
+				break;
+			}
 		}
 		
-		if(records.size()==0) {
+		
+		// 결과 출력
+		if (isflag) {
 			System.out.println("Possible");
 		} else {
 			System.out.println("Impossible");
 		}
-    }
+
+	} // main
 }

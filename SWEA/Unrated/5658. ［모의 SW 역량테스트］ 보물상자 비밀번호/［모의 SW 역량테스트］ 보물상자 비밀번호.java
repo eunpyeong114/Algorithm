3dff277dc1fb5c3c2796db44e5008c17d7067a1c
eunpyeong114 <139293873+eunpyeong114@users.noreply.github.com>
2023-09-18 15:41:44 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 153 ms, Memory: 24,228 KB -BaekjoonHub

---
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -0,0 +1,48 @@
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			Set<String> set = new HashSet<>();
+			char[] inputs = new char[N * 2];
+			int front = 0;
+			int rear = N - 1;
+			char[] inputStr = sc.next().toCharArray();
+			for (int i = 0; i < inputStr.length; i++) {
+				inputs[i] = inputStr[i];
+			}
+			int cnt = N / 4;
+			// 시계방향으로 돌리기 (cnt번)
+			for (int i = 0; i < cnt; i++) {
+				char tmp = inputs[front++];
+				inputs[++rear] = tmp;
+				// 돌릴 때마다 4변에 존재하는 숫자값 구하기
+				for (int j = 0; j < 4; j++) {
+					String str = "";
+					// 글자자르기
+					for (int k = 0; k < cnt; k++) {
+						str += inputs[front + j * cnt + k];
+					}
+					set.add(str);
+				}
+			}
+			Integer[] arr = new Integer[set.size()];
+			int idx = 0;
+			for (String s : set) {
+				arr[idx++] = Integer.parseInt(s, 16);
+			}
+			Arrays.sort(arr, Collections.reverseOrder());
+			System.out.println("#"+tc+" " +arr[K-1]);
+		}
+	}
+}

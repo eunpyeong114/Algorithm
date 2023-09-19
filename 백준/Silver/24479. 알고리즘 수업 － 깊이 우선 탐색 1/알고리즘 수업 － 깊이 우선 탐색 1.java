deb43c2a0ef59d3f6362c43768a759942b27b7ac
eunpyeong114 <139293873+eunpyeong114@users.noreply.github.com>
2023-09-19 12:33:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1100 ms, Memory: 116748 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+	public static ArrayList<Integer>[] graph;
+	public static boolean[] visited;
+	public static int cnt = 0;
+	public static int[] order;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] firstInput = br.readLine().split(" ");
+		// 정점의 갯수
+		int N = Integer.parseInt(firstInput[0]);
+		// 간선의 갯수
+		int M = Integer.parseInt(firstInput[1]);
+		// 시작 정점
+		int R = Integer.parseInt(firstInput[2]);
+
+		// 인접리스트
+		graph = new ArrayList[N + 1];
+
+		// 방문 기록
+		visited = new boolean[N + 1];
+
+		// 방문 순서
+		order = new int[N + 1];
+
+		for (int i = 1; i < N + 1; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
+
+		for (int i = 1; i <= M; i++) {
+			String[] otherInput = br.readLine().split(" ");
+			int u = Integer.parseInt(otherInput[0]);
+			int v = Integer.parseInt(otherInput[1]);
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+
+		for (int i = 1; i < graph.length; i++) {
+			Collections.sort(graph[i]);
+		}
+		
+		DFS(R);
+
+		for (int i = 1; i < order.length; i++) {
+			bw.write(Integer.toString(order[i]));
+			bw.newLine();
+		}
+		bw.flush();
+	}
+
+	public static void DFS(int v) {
+		if (visited[v]) {
+			return;
+		}
+		cnt++;
+		visited[v] = true;
+		order[v] = cnt;
+
+		for (int i : graph[v]) {
+			if (!visited[i]) {
+				DFS(i);
+			}
+		}
+	}
+}

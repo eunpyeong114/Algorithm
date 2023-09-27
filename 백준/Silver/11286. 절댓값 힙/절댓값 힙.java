ddf0d8b3e2894aaed84db545458f125d0487a886
eunpyeong114 <139293873+eunpyeong114@users.noreply.github.com>
2023-09-27 09:26:54 +0900

    [Silver I] Title: 절댓값 힙, Time: 812 ms, Memory: 96012 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,37 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		PriorityQueue<Long> pq = new PriorityQueue<>(new Comparator<Long>() {
+			@Override
+			public int compare(Long o1, Long o2) {
+				int result = (int) (Math.abs(o1) - Math.abs(o2));
+				if (result == 0) {
+					return (int) (o1 - o2);
+				} else {
+					return result;
+				}
+			}
+		});
+
+		int N = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			long input = sc.nextInt();
+			if (input == 0) {
+				if (pq.isEmpty()) {
+					sb.append("0\n");
+				} else {
+					sb.append(pq.poll() + "\n");
+				}
+			} else {
+				pq.add(input);
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
